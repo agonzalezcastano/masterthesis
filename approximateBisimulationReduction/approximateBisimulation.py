dc8f6466d20860cfc4e846a5246ceb10d81e96f8
agonzalezcastano <a.gonzalez.castano@fz-juelich.de>
2021-12-21 15:05:12 +0100

    Finished bisimulation

--- approximateBisimulationReduction/approximateBisimulation.py
+++ approximateBisimulationReduction/approximateBisimulation.py
@@ -1,22 +1,24 @@
 import numpy as np
 import scipy.linalg as linalg
+from scipy.linalg import decomp_cholesky
 import linearSystem
 from linearSystem import LinearSystem
 import accuracy
+import decompositionSystem
 
 def algorithm(f: function, g: function, inputs, initial_states, reduced_order, states_max, delta_max, error_tolerance):
     A, B, C, inputs, states = linearSystem.systemLinearization(f, g, initial_states, reduced_order)
     G_1 = LinearSystem(A, B, C, inputs, states)
-    G_1_stable: LinearSystem = extractStableSystem(G_1, reduced_order)
-    H: np.ndarray = calculateSurjectiveMap()
+    G_1_stable, projection = extractStableSystem(G_1, reduced_order)
+    H: np.ndarray = calculateSurjectiveMap(G_1_stable, projection)
     G_r_2: LinearSystem = reduction(G_1_stable, H)
     states_r = linearSystem.solveLinearSystem(G_r_2)
 
     while  accuracy.isAccuracyCriterionSatisfied(states_r, states_max):
         A, B, C, inputs, states = linearSystem.systemLinearization(f, g, initial_states, reduced_order)
         G_1 = LinearSystem(A, B, C, inputs, states)
-        G_1_stable: LinearSystem = extractStableSystem(G_1, reduced_order)
-        H: np.ndarray = calculateSurjectiveMap()
+        G_1_stable, projection = extractStableSystem(G_1, reduced_order)
+        H: np.ndarray = calculateSurjectiveMap(G_1_stable, projection)
         G_r_2: LinearSystem = reduction(G_1_stable, H)
         states_r = linearSystem.solveLinearSystem(G_r_2)
         p_r_2 = np.dot(G_r_2.C, states_r)
@@ -40,9 +42,10 @@ def extractStableSystem(G : LinearSystem, reduced_order):
     if n_unstable == reduced_order:
         # System is unstable
         # Extraction of the stable subsystem
-        A_stable = A[(n_unstable + 1):n, (n_unstable + 1):n]
-        B_stable = B[(n_unstable + 1):n, :]
-        C_stable = C[:, (n_unstable + 1):n]
+        G_s, projection = decompositionSystem.decomposition(G)
+        A_stable = G_s.A
+        B_stable = G_s.B
+        C_stable = G_s.C
     if n_unstable == 0:
         # System is stable
         A_stable = A
@@ -51,7 +54,7 @@ def extractStableSystem(G : LinearSystem, reduced_order):
 
     G_stable = LinearSystem(A_stable, B_stable, C_stable, inputs, initial_states)
 
-    return G_stable
+    return G_stable, projection
 
 def reduction(G_stable: LinearSystem, H: np.ndarray):
     H_inv = linalg.inv(H)
@@ -59,30 +62,36 @@ def reduction(G_stable: LinearSystem, H: np.ndarray):
     B_r = np.dot(H, G_stable.B)
     C_r = np.dot(G_stable.C, H_inv)
 
-    inputs = np.dot(H, G_stable.inputs)
-    states = G_stable.initial_states
+    states = np.dot(H, G_stable.initial_states)
+    inputs = G_stable.inputs
 
     G_r = LinearSystem(A_r, B_r, C_r, inputs, states)
     return G_r
 
 def solveLyapunovEquations(A, C):
-    eigenvalue_min = min(- np.real(linalg.eig(A)))/1.01
+    lambda_min = min(- np.real(linalg.eig(A)))
     A_identity = np.identity(np.size(A))
+    lamba_min_matrix = np.dot(lambda_min, A_identity)
 
     CC= np.dot(np.transpose(C), C)
-    Q = (np.transpose(A) +
-        np.dot(eigenvalue_min * A_identity) * CC) + np.dot(CC, (A + eigenvalue_min * A_identity))
-    [V, D] = linalg.eig(Q)
-    D = np.diag(np.dot((np.diag(D)>0), np.diag(D)))
-    Q = np.dot(np.dot(V, D), np.transpose(V))
-    M = CC + linalg.solve_continuous_lyapunov(np.transpose(A) + eigenvalue_min * A_identity, Q)
+    Q = (np.transpose(A) + np.dot(lamba_min_matrix, CC)) + np.dot(CC, (A + lamba_min_matrix))
+    eig_vals, right_eig_vecs = linalg.eig(Q, right = True)
+    diag_eig_vals = np.diag(eig_vals)
+    D = np.diag(np.dot((np.diag(diag_eig_vals)>0), np.diag(diag_eig_vals)))
+    Q = np.dot(np.dot(right_eig_vecs, D), np.transpose(right_eig_vecs))
+    M = CC + linalg.solve_continuous_lyapunov(np.transpose(A) + lamba_min_matrix, Q)
 
     return M
 
-def calculateBisimulationFunction()
-    Vs =
-    return Vs
+def calculateSurjectiveMap(m, k, n_unstable):
+    #  Hs is such that the eigenvalues of the matrix Hs * As,1 * Hs+ have all a strictly negative real part
 
-def calculateSurjectiveMap()
-    H = 
+    zero_matrix = np.zeros(k-n_unstable, n_unstable)
+    identity_matrix_m = np.identity(n_unstable, m)
+    identity_matrix_k= np.identity(k-n_unstable, m-n_unstable)
+    matrix = np.array([identity_matrix_m],
+                        [zero_matrix, identity_matrix_k])
+
+    H = matrix
+    
     return H
--- approximateBisimulationReduction/linearSystem.py
+++ approximateBisimulationReduction/linearSystem.py
@@ -8,7 +8,6 @@ class LinearSystem:
         self.A = 0
         self.B = 0
         self.C = 0
-        self.D = 0
         self.inputs = 0
         self.initial_states = 0
 
@@ -23,11 +22,10 @@ def systemLinearization(f: function, g: function, initial_states, n):
     A = diff_f_x - np.dot(np.dot(diff_f_u, inv(diff_g_u)), diff_g_x)
     B = np.identity(n)
     C = diff_g_x
-    D = diff_g_u
     
     inputs = f(initial_states.x, initial_states.u) - (np.dot(A, initial_states.x))
 
-    lin_model: LinearSystem = LinearSystem(A, B, C, D, inputs, initial_states)
+    lin_model: LinearSystem = LinearSystem(A, B, C, inputs, initial_states)
 
     return lin_model
 
@@ -50,7 +48,7 @@ def partialDiffOfGY(g, u, x_0, u_0):
 def solveLinearSystem(G_r: LinearSystem):
     x = sp.symbols('x')
     f: function = (np.dot(G_r.A, x)) + (np.dot(G_r.B, G_r.inputs))
-    g: function = (np.dot(G_r.C, x)) + (np.dot(G_r.D, G_r.inputs))
+    g: function = np.dot(G_r.C, x)
 
     states_r = solve((f, g), x)
     return states_r
