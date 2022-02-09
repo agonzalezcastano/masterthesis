5b57597af9ad89083583505eba5e53b4f725f33a
agonzalezcastano <a.gonzalez.castano@fz-juelich.de>
2022-02-09 22:30:33 +0100

    changes

--- spectralProjectionModalTruncationAlgorithm/spectralProjection.py
+++ spectralProjectionModalTruncationAlgorithm/spectralProjection.py
@@ -2,6 +2,6 @@ import numpy as np
 
 def calculateSpectralProjection(A: np.ndarray, alpha: np.ndarray):
     I = np.identity(np.shape(A)[0])
-    P = np.sign(A + (alpha * I))
+    P = 1/2 * (I + np.sign(A + (alpha * I)))
     
     return P
